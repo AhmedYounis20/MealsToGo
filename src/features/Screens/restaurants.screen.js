292f0d9f432a77de9fbc50a5385767859a550891
AhmedYounis20 <ahmed.m.younis20@gmail.com>
2023-04-08 00:02:42 +0200

    cleanup && adding styles

--- src/features/Screens/restaurants.screen.js
+++ src/features/Screens/restaurants.screen.js
@@ -7,25 +7,25 @@ import {
   Platform,
 } from "react-native";
 import { Searchbar } from "react-native-paper";
-import { RestaurantInfoCard } from "../Components/restaurant-info-card.Component";
-import { restaurants } from "../../testData/restaurants";
+import { RestaurantInfoCard } from "../restaurants/Components/restaurant-info-card.Component";
+import { restaurants } from "../../../testData/restaurants";
 import { FlatList } from "react-native";
 import styled from "styled-components";
 
 const SafeArea = styled(SafeAreaView)`
   flex: 1;
-  background-color: "#fff";
+  background-color: ${(props)=>props.theme.colors.bg.primary};
   ${StatusBar.currentHeight && `margin-top:${StatusBar.currentHeight}px`};
-  margin-top: ${Platform.OS === "android" ? StatusBar.currentHeight : 0}px;
+  
 `;
 
 const SearchContainer = styled(View)`
-  padding: 16px;
+  padding: ${(props)=>props.theme.space[3]};
 `;
 
 const ListContainer = styled(View)`
   flex: 1;
-  padding: 16px;
+  padding: ${(props)=>props.theme.space[3]};
 `;
 
 export const RestaurantsScreen = () => {
