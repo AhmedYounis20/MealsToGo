498197846a036be9b7ccdda4aa5d1245a35b766b
AhmedYounis20 <ahmed.m.younis20@gmail.com>
2023-04-28 21:46:24 +0300

    update used packages

--- App.js
+++ App.js
@@ -1,23 +1,37 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 import { ThemeProvider } from "styled-components/native";
-import {theme} from "./src/infrastructure/theme"
-import { Oswald_400Regular, useFonts as useOswald} from "@expo-google-fonts/oswald";
+import { theme } from "./src/infrastructure/theme";
+import {
+  Oswald_400Regular,
+  useFonts as useOswald,
+} from "@expo-google-fonts/oswald";
 import { Lato_400Regular, useFonts as useLato } from "@expo-google-fonts/lato";
-import { RestaurantsScreen } from "./src/features/restaurants/Screens/restaurants.screen";
+import { Navigation } from "./src/infrastructure/navigation";
+import { initializeApp,getApps } from "firebase/app";
+import { AuthenticationContextProvider } from "./src/services/authentication/authentication.context";
+import { firebaseConfig } from "./src/services/authentication/authentication.mock";
+
+
+if (!getApps().length) initializeApp(firebaseConfig);
+
 export default function App() {
-  // const [seachValue, setSearchValue] = useState("");
-  // const OnSearchChanged = (query) => setSearchValue(query);
-  const [oswaldLoded] = useOswald({Oswald_400Regular});
-  const [latoLoded] = useLato({Lato_400Regular});
-  if(!oswaldLoded || !latoLoded)
+  const [oswaldLoded] = useOswald({ Oswald_400Regular });
+  const [latoLoded] = useLato({ Lato_400Regular });
+  if (!oswaldLoded || !latoLoded) {
     return null;
+  }
+  
   return (
     <>
-    <ThemeProvider theme={theme}>
-      <RestaurantsScreen />
-    </ThemeProvider>
-      <ExpoStatusBar style="dark" />
+      <ThemeProvider theme={theme}>
+        <AuthenticationContextProvider>
+
+              <Navigation />
+              <ExpoStatusBar style="dark" />
+   
+        </AuthenticationContextProvider>
+      </ThemeProvider>
     </>
   );
 }
